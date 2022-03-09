7f4d8565f12618806d56e22f1aef0a8e378622f7
Daniele Arruda <83968716+DanieleArrud@users.noreply.github.com>
2022-03-09 09:30:24 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,221 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
+import { AppLoading }  from 'expo';
+import { AntDesign } from '@expo/vector-icons';
+// import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
+import { StyleSheet, ScrollView, Text, View, ImageBackground, TouchableOpacity, TouchableHighlight ,Modal ,TextInput } from 'react-native';
+import { Alert } from 'react-native-web';
+import {AsyncStorage } from 'react-native';
+
+export default function App() {
+
+  const image = require('./resources/bg.jpg');
+
+  console.disableYellowBox = true;
+  
+  const [tarefas, setarTarefas] = useState([ ]); 
+
+  const [ tarefaAtual, setTarefaAtual] =  useState('');
+
+  const [modal, setModal] = useState(false);
+
+  useEffect(()=>{
+    //Quando inicar, leia.
+    (async() =>{
+      try{
+        let tarefasAtual = await AsyncStorage.getItem('tarefas');
+        if(tarefaAtual == null)
+        setTarefaAtual([]);
+        else
+        setarTarefas(JSON.perse(tarefasAtual));
+      }catch(error){
+        //Erro
+      }
+    })();
+
+  },[])
+
+   // let [fontsLoaded] = useFonts({
+  //   Lato_400Regular,
+  // });
+
+  // if(!fontsLoaded){
+  //   return <AppLoading />;
+  // }
+
+  function deletarTarefa(id){
+    let newTarefas = tarefas.filter(function(val){
+      return val.id != id;
+    });
+    setarTarefas(newTarefas); 
+
+    (async () => {
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify(newTarefas));
+        console.log('chamado');
+      } catch (error){
+        //Error saving date
+      }
+    })();  
+  }
+
+  function addTarefa(){
+    setModal(!modal);
+
+    let id = 0;
+    if(tarefas.length > 0){
+      id = tarefas[tarefas.length - 1].id + 1;
+    }
+
+    let tarefa = {id:id,tarefa:tarefaAtual};
+
+    setarTarefas([...tarefas,tarefa]);
+
+    (async () => {
+      try {
+        await AsyncStorage.setItem('tarefas', JSON.stringify([...tarefas,tarefa]));
+      } catch (error){
+        //Error saving date
+      }
+    })();  
+  }
+
+
+  return (
+
+    <ScrollView style={{flex:1}}>
+
+      <StatusBar hidden/>
+
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed");
+        }}
+        >
+
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <TextInput onChangeText={text => setTarefaAtual(text)} autoFocus={true}></TextInput>
+
+              <TouchableHighlight style={{ ...styles.openButton, backgroundColor: "#010105"}} 
+              onPress={() => addTarefa()}>
+
+                <Text style={styles.textStyle}>Adicionar Tarefa</Text>
+                </TouchableHighlight>
+              </View>
+
+            </View>
+          </Modal>
+
+        <ImageBackground source={image} style={styles.image}>
+         <View style={styles.coverView}>
+            <Text style={styles.textHeader}>Lista de Tarefas</Text>
+          </View>
+        </ImageBackground> 
+
+      {
+        tarefas.map(function(val){
+          return (<View style={styles.tarefaSingle}>
+     
+              <View style={{flex:1,width:'100%', padding:10}}>
+                <Text>{val.tarefa}</Text>
+              </View>
+
+              <View style={{alignItems: 'flex-end', flex:1, padding: 10}}>
+                <TouchableOpacity onPress={()=> deletarTarefa(val.id)}>
+                  <AntDesign name="minuscircleo" size={24} color="black" />
+                </TouchableOpacity>
+              </View>
+
+          </View>);
+        })
+
+     }
+
+     <TouchableOpacity style={styles.btnAddTarefa} onPress={()=>setModal(true)}>
+       <Text style={{textAlign:'center', padding: 10, color:'white'}}>Adicionar Tarefa</Text>
+    </TouchableOpacity>
+         
+    </ScrollView>
+  
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width:'100%',
+    height: 80,
+    resizeMode:"cover"
+  },
+  btnAddTarefa: {
+    width: 200,
+    padding: 8,
+    backgroundColor: 'grey',
+    marginTop: 20
+  },
+  coverView:{
+    width: '100%',
+    height: 80,
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+
+  textHeader:{
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+    marginTop: 20,
+    // fontFamily: "Lato_400Regular"
+  },
+
+  tarefaSingle:{
+    marginTop: 30,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    flexDirection: "row",
+    paddingBottom: 10
+  },
+
+  //MODAL
+
+  centeredView:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
